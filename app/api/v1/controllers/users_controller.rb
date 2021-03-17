d87bfdb82fb191ba18a7d2d49c6330a4e50d1d89
denalibalser <denali.balser167@gmail.com>
2021-03-17 10:50:28 -0600

    added actions to users controller

--- app/api/v1/controllers/users_controller.rb
+++ app/api/v1/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
-
+    #add before_action's for authorization 
+    
+    def create 
+        @user = User.create(user_params)
+        render json: @user
+    end 
+    
     def show 
         @user = User.find_by_id(params[:id])
         render json: @user
     end 
 
     private 
-    
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :password)
+    end
 end

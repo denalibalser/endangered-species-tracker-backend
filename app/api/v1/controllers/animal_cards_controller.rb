08db178d23dd463a090342d12a8d9ecfab2a8649
denalibalser <denali.balser167@gmail.com>
2021-03-17 10:57:43 -0600

    added actions to animalcard controller

--- app/api/v1/controllers/animal_cards_controller.rb
+++ app/api/v1/controllers/animal_cards_controller.rb
@@ -1,2 +1,30 @@
 class AnimalCardsController < ApplicationController
+
+    def create 
+        @animalcard = AnimalCard.create(animalcard_params)
+        render json: @animalcard
+    end 
+
+    def show 
+        @animalcard = AnimalCard.find_by_id(params[:id])
+        render json: @animalcard
+    end 
+    
+    def index 
+        @animalcards = AnimalCard.all 
+        render json: @animalcards
+    end 
+
+    def destroy 
+        @animalcard = AnimalCard.find_by_id(params[:id])
+        @animalcard.destroy
+    end 
+
+    private
+
+    def animalcard_params 
+        params.require(:animalcard).permit(:common_name, :scientific_name, :endangered_level, :population_trend, :assessment_date)
+    end
+
 end
+
